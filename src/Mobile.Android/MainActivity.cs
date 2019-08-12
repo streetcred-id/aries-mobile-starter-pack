e65f595d54ddc3696013d2674b9152790b52bc11
Tomislav Markovski <tomislav@streetcred.id>
2019-08-11 21:48:37 -0400

    Add starter project

---
+++ src/Mobile.Android/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Android;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mobile.Droid
+{
+    [Activity(Label = "Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            global::Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            //Loading dependent libindy
+            JavaSystem.LoadLibrary("gnustl_shared");
+            JavaSystem.LoadLibrary("indy");
+
+            // Android requires that we set content root.
+            var host = App.BuildHost()
+                .UseContentRoot(System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal)).Build();
+
+            var application = host.Services.GetRequiredService<App>();
+
+            LoadApplication(application);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ src/Mobile/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Mobile.Services;
+using Mobile.Views;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading;
+
+namespace Mobile
+{
+    public partial class App : Application
+    {
+        public App() => InitializeComponent();
+
+        public App(IHost host) : this() => Host = host;
+
+        public static IHost Host { get; private set; }
+
+        public static IHostBuilder BuildHost() =>
+            XamarinHost.CreateDefaultBuilder<App>()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddScoped<MainPage>();
+                    services.AddAgentFramework();
+                });
+
+        protected override void OnStart()
+        {
+            Host.Start();
+            MainPage = Host.Services.GetRequiredService<MainPage>();
+        }
+
+        protected override void OnSleep()
+        {
+            Host.Sleep();
+        }
+
+        protected override void OnResume()
+        {
+            Host.Resume();
+        }
+    }
+}
